32e7616b805e4c1abefa3da17447d7ba2ef79c73
tmcn99 <tmcn134@gmail.com>
2025-11-13 11:49:32 +0100

    0.1.6
    Api Create metode

---
+++ ReelWorld.Api/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Profiles = ReelWorld.DataAccessLibrary.Model;
+using ReelWorld.DataAccessLibrary.Interfaces;
+using ReelWorld.DataAccessLibrary.Model;
+
+
+namespace ReelWorld.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        IUserDao _userDao;
+        public UsersController(IUserDao userDao)
+        {
+            _userDao = userDao;
+        }
+
+        [HttpPost]
+        public ActionResult<int> Create(User user)
+        {
+            try
+            {
+                return Ok(_userDao.Create(user));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+    }
+
+}
