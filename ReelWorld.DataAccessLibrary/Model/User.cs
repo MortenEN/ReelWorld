6dfd6bf63f3ff1a90b531701ca807f19e026b29a
Andreas-SB <10632087@ucn.dk>
2025-11-13 11:23:59 +0100

    V0.1.2
    User cs og con

---
+++ ReelWorld.DataAccessLibrary/Model/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReelWorld.DataAccessLibrary.Model
+{
+    public class User : Profile
+    {
+        public int userId { get; set; }
+        public string phoneNo { get; set; }
+        public int age { get; set; }
+
+        public enum realtionship
+        {
+            Single,
+            Taken,
+            complicated,
+        }
+        public List<String> Interests { get; set; }
+        public string description { get; set; }
+
+        public User(string name, string email, string hashPassword, int userId, string phoneNo, int age, List<string> interests, string description):base(name,email,hashPassword)
+        {
+            this.name = name;
+            this.email = email;
+            this.hashPassword = hashPassword;
+            this.userId = userId;
+            this.phoneNo = phoneNo;
+            this.age = age;
+            Interests = interests;
+            this.description = description;
+        }
+    }
+}
