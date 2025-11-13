ee33a70bd800a88fe608493185e2fe90db5d003d
Andreas-SB <10632087@ucn.dk>
2025-11-13 09:48:04 +0100

    V 0.1.1
    Mapper og profil cs

---
+++ ReelWorld.DataAccessLibrary/Model/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReelWorld.DataAccessLibrary.Model
+{
+    abstract class Profile
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
