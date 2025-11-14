3ce91f1b5d677410fc2969784b3ad4f2181327f3
benjaminwalther9490 <10632096@ucn.dk>
2025-11-14 08:50:53 +0100

    0.1.8
    Tilføjelse af BCrypt samt sql script

---
+++ ReelWorld.DataAccessLibrary/Tools/BCryptTool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bcryptPackage = BCrypt.Net;
+
+namespace ReelWorld.DataAccessLibrary.Tools
+{
+    public class BCryptTool
+    {
+        public static string HashPassword(string password) =>
+        bcryptPackage.BCrypt.HashPassword(password, GetRandomSalt());
+        public static bool ValidatePassword(string password, string correctHash) => bcryptPackage.BCrypt.Verify(password, correctHash);
+        private static string GetRandomSalt() => bcryptPackage.BCrypt.GenerateSalt(12);
+    }
+}
