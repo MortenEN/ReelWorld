ae480b26dd46c90d79f9ea4660bf64b1f3f62a0f
tmcn99 <tmcn134@gmail.com>
2025-11-17 12:09:22 +0100

    0.1.30
    Event Test

--- ReelWorld.Test/EventTests/EventDaoTest.cs
+++ ReelWorld.Test/EventTests/EventDaoTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using ReelWorld.DataAccessLibrary.Interfaces;
 using ReelWorld.DataAccessLibrary.Model;
+using ReelWorld.DataAccessLibrary.SqlServer;
 using ReelWorld.DataAccessLibrary.Stub;
 
 namespace ReelWorld.Test;
@@ -8,6 +9,8 @@ namespace ReelWorld.Test;
 public class EventDaoTest
 {
     private IEventDao _dao;
+    private const string connectionsString = "Data Source=hildur.ucn.dk;Initial Catalog=DMA-CSD-S241_10632087;Persist Security Info=True;User ID=DMA-CSD-S241_10632087;Password=Password1!;Encrypt=True;Trust Server Certificate=True";
+
 
     [SetUp]
     public void Setup()
@@ -25,5 +28,25 @@ public class EventDaoTest
         //assert
         Assert.That(createdId, Is.EqualTo(1));
         Assert.That(Event.EventId, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task EventDao_Create_Event_With_Database()
+    {
+        //arrange
+        EventDao eventDao = new(connectionsString);
+        var TestEvent = new Event
+        {
+            Title = "Test Event",
+            Description = "This is a test event.",
+            Location = "Test Location",
+            Date = DateTime.Now,
+            IsPublic = true
+        };
+        //act
+        var createdId = await eventDao.CreateEventAsync(TestEvent);
+        //assert
+        Assert.That(createdId, Is.GreaterThan(0), "The Create method should return a EventId that is above 0");
+
     }
 }
