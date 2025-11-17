2d6af47b526a78904b4d3bb9a5204f8882e09da1
Andreas-SB <10632087@ucn.dk>
2025-11-17 09:57:15 +0100

    0.1.25
    Test med stub

--- ReelWorld.Test/EventTests/EventDaoTest.cs
+++ ReelWorld.Test/EventTests/EventDaoTest.cs
@@ -1,15 +1,29 @@
-﻿namespace ReelWorld.Test;
+﻿using Microsoft.Extensions.Logging;
+using ReelWorld.DataAccessLibrary.Interfaces;
+using ReelWorld.DataAccessLibrary.Model;
+using ReelWorld.DataAccessLibrary.Stub;
+
+namespace ReelWorld.Test;
 
 public class EventDaoTest
 {
+    private IEventDao _dao;
+
     [SetUp]
     public void Setup()
     {
+        _dao = new InMeMoryEventDaoStub();
     }
 
     [Test]
-    public void Test1()
+    public void EventDao_Create_Event_With_Stub()
     {
-        Assert.Pass();
+        //arrange
+        var Event = new Event { EventId = 0, Title = "test" };
+        //act
+        var createdId = _dao.Create(Event);
+        //assert
+        Assert.That(createdId, Is.EqualTo(1));
+        Assert.That(Event.EventId, Is.EqualTo(1));
     }
 }
