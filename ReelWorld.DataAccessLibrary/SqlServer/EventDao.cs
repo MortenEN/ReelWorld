5257115cc9833d216a8d9eb2235151314657ded5
Andreas-SB <10632087@ucn.dk>
2025-11-17 11:45:28 +0100

    0.1.28
    EventDao

---
+++ ReelWorld.DataAccessLibrary/SqlServer/EventDao.cs
@@ -0,0 +1,48 @@
+﻿using ReelWorld.DataAccessLibrary.Interfaces;
+using ReelWorld.DataAccessLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReelWorld.DataAccessLibrary.SqlServer
+{
+    public class EventDao : BaseDao, IEventDao
+    {
+        public EventDao(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public int Create(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CreateEventAsync(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int eventid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Event> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event? GetOne(int eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
