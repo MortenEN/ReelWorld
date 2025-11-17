65142d3ac555e5bcf56ceceb2fc2f24cf8e3cd15
Andreas-SB <10632087@ucn.dk>
2025-11-17 09:24:10 +0100

    0.1.20
    Event model

---
+++ ReelWorld.DataAccessLibrary/Model/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReelWorld.DataAccessLibrary.Model
+{
+    internal class Event
+    {
+        #region Properties
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string Type { get; set; }
+        #endregion
+
+        #region Constructor
+        public Event(int eventId, string title, string description, DateTime date, string location, string type)
+        {
+            EventId = eventId;
+            Title = title;
+            Description = description;
+            Date = date;
+            Location = location;
+            Type = type;
+        }
+        public Event()
+        {
+
+        }
+        #endregion
+    }
+}
