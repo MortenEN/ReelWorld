af1e17b4b38911d5ffecc9f2ceadd52c03d9054b
tmcn99 <tmcn134@gmail.com>
2025-11-17 09:37:15 +0100

    0.1.21
    Api og interface

---
+++ ReelWorld.Api/Controllers/EventsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReelWorld.DataAccessLibrary.Interfaces;
+using ReelWorld.DataAccessLibrary.Model;
+using ReelWorld.DataAccessLibrary.SqlServer;
+
+namespace ReelWorld.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        IEventDao _eventDao;
+        public EventsController(IEventDao eventDao)
+        {
+            _eventDao = eventDao;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(Event event)
+        {
+            try
+            {
+                var id = await _eventDao.CreateEventAsync(event);
+                return Ok(id);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ReelWorld.DataAccessLibrary/Interfaces/IEventDao.cs
@@ -0,0 +1,26 @@
+﻿using ReelWorld.DataAccessLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReelWorld.DataAccessLibrary.Interfaces
+{
+    public interface IEventDao
+    {
+        Event? GetOne(int id);
+
+        IEnumerable<Event> GetAll();
+
+        int Create(Event event);
+
+        bool Update(Event event);
+
+        bool Delete(int id);
+
+        Task<int> CreateEventAsync(Event event);
+
+        }
+    }
+}
