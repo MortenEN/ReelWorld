26fd4cf2ab7ffecb1f96e2404adeb03ec215685b
tmcn99 <tmcn134@gmail.com>
2025-11-20 11:56:17 +0100

    0.2.13
    Unit test for delete profile

--- ReelWorld.Test/ProfileTests/ProfileDaoTest.cs
+++ ReelWorld.Test/ProfileTests/ProfileDaoTest.cs
@@ -76,8 +76,45 @@ namespace ReelWorld.Test.ProfileTests
             // Assert
             Assert.That(newProfileId, Is.GreaterThan(0), "The Create method should return a ProfileID that is above 0");
 
-            ////Cleanup
-            //bool deleteResult = await profileDao.DeleteAsync(newProfileId);
+            //Cleanup
+            bool deleteResult = await profileDao.DeleteAsync(newProfileId);
+        }
+
+
+
+        [Test]
+        public async Task ProfileDao_Delete_Profile_With_Database()
+        {
+            // Arrange
+            ProfileDao profileDao = new(connectionsString);
+
+            List<string> interests = new List<string> { "Paddle", "Reading" };
+
+            Profile profile = new Profile
+            {
+                Name = "Test_ProfileDao_Create_Profile_With_Database",
+                Email = "test@testing.testtest",
+                HashPassword = "12345",
+                PhoneNo = "12345678",
+                Age = 26,
+                Relationship = Profile.RelationshipStatus.Single,
+                Description = "Test test test",
+                CityName = "Aalborg",
+                CountryName = "Denmark",
+                StreetName = "Gaden",
+                StreetNumber = "17",
+                ZipCode = "9000",
+                Interests = interests
+            };
+
+            // Act
+            int id = await profileDao.CreateAsync(profile);
+            bool deleteResult = await profileDao.DeleteAsync(id);
+
+            // Assert
+            Assert.Greater(id, 0, "CreateAsync should return a valid ID");
+            Assert.IsTrue(deleteResult, "DeleteAsync should return true for a valid profile");
+
         }
 
     }
