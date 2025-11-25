23040576c62b25738a6e04abfae5d0357e863be8
MortenEN <10632125@ucn.dk>
2025-11-25 10:28:03 +0100

    0.2.46
    test til registration og concurrency

--- ReelWorld.Test/RegistrationTests/RegistrationDaoTest.cs
+++ ReelWorld.Test/RegistrationTests/RegistrationDaoTest.cs
@@ -19,12 +19,16 @@ public class RegistrationDaoTest
     {
         //arrange
         var registrationDao = new RegistrationDao(connectionsString);
+        var eventDao = new EventDao(connectionsString);
 
-        int testEventId = 1; // make sure event exists in test DB
+        var attendees = new List<Profile>();
+        Event @event = new Event(0, "Test for joinAsync", "For testing joinAsync", DateTime.Now, "in .test", true, 1, 1, attendees);
+        int eventId = await eventDao.CreateAsync(@event);
+
         int testUserId = 1;  // make sure user exists in test DB
 
         //act
-        var result = await registrationDao.JoinEventAsync(testEventId, testUserId);
+        var result = await registrationDao.JoinEventAsync(eventId, testUserId);
         //assert
         Assert.That(result, Is.True, "JoinEventAsync should return true when user successfully joins an event.");
     }
@@ -34,22 +38,21 @@ public class RegistrationDaoTest
         //arrange
         var registrationDao = new RegistrationDao(connectionsString);
         var eventDao = new EventDao(connectionsString);
+
         var attendees = new List<Profile>();
+        Event @event = new Event(0, "Test for concurrency", "For testing concurrency", DateTime.Now, "in .test", true, 1, 1, attendees);
+        int eventId = await eventDao.CreateAsync(@event);
 
-        Event @event = new Event(6969, "Test for concurrency", "For testing concurrency", DateTime.Now, "in .test", true, 1, 1, attendees);
-        await eventDao.CreateAsync(@event);
-
-        int testEventId = @event.EventId; // make sure event exists in test DB
-        int testUserId0 = 1;  // make sure user exists in test DB
-        int testUserId1 = 2;  // make sure user exists in test DB
+        int testUserId0 = 1;
+        int testUserId1 = 2;
 
         //act
-        var result0 = registrationDao.JoinEventAsync(testEventId, testUserId0);
-        var result1 = registrationDao.JoinEventAsync(testEventId, testUserId1);
+        var result0 = registrationDao.JoinEventAsync(eventId, testUserId0);
+        var result1 = registrationDao.JoinEventAsync(eventId, testUserId1);
 
         await Task.WhenAll(result0, result1);
-
+        var evt = await eventDao.GetOneAsync(eventId);
         //assert
-        Assert.That(@event.AttendeeCount == 1, Is.True, "Only one profile joined the event.");
+        Assert.That(evt.AttendeeCount == 1, Is.True, "Only one profile joined the event.");
     }
 }
